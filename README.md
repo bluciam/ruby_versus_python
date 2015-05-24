a41b01147aff43faad4df2cd29a63a0ab5e8334a
Blanca Mancilla <blancalmancilla@gmail.com>
2015-05-24 14:03:58 -0400

    Blanca finished Kap1!

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 
 Side by side programming
 
-Sophie and Blanca want to work on core problems in programming each with a slight different porspective: Sophie will work in Ruby and Blanca in Python.
+Sophie and Blanca want to work on core problems in programming each with a
+slight different perspective: Sophie will work in Ruby and Blanca in Python.
---
+++ white_book/Kap_1/1_8.py
@@ -0,0 +1,25 @@
+def isSubstring(sub_s1, s1):
+    if sub_s1 in s1:
+        return True
+    else:
+        return False
+
+def isRotation(s1, s2):
+    c = s1[0]
+    substr2_p1 = []
+    substr2_p2 = []
+    for i, value in enumerate(s2):
+        if c != value:
+            substr2_p1.append(value)
+        else:
+            for char_i in range(i,len(s2)):
+                substr2_p2.append(s2[char_i])
+            break
+
+    return isSubstring(s1, ''.join(substr2_p2) + ''.join(substr2_p1))
+
+
+print isRotation("hello", "lohel")
+print isRotation("hello", "lohell")
+
+
